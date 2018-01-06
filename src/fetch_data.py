6ab69f71d410c9452132f5768bfa7149da71b18e
Erik Rosvall <erik.rosvall@gmail.com>
2018-01-06 12:03:02 +0100

    Kan nu hämta ut hela ModeApt spliten av datan för approximationenskernelen

--- src/fetch_data.py
+++ src/fetch_data.py
@@ -1,7 +1,7 @@
 from nltk.corpus import reuters, stopwords
 import random, re
 
-def cleanData(txt):
+def _cleanData(txt):
 	#lowercase
 	txt = txt.lower()
 	#keep only words with letters (because f*ck numbers amirite?!)
@@ -21,10 +21,21 @@ def loadData(categories, nrTrain, nrTest, shuffle = True):
 		k = 0
 		while(len(trainingData) < sum(nrTrain[:i+1]) or len(testData) < sum(nrTest[:i+1])):
 			if ids[k].startswith('train') and len(trainingData) < sum(nrTrain[:i+1]): 
-				trainingData.append([ cleanData(reuters.raw(ids[k])), cat])
+				trainingData.append([ _cleanData(reuters.raw(ids[k])), cat])
 			elif ids[k].startswith('test') and len(testData) < sum(nrTest[:i+1]):
-				testData.append([ cleanData(reuters.raw(ids[k])), cat]) 
+				testData.append([ _cleanData(reuters.raw(ids[k])), cat]) 
 			k += 1
+	return trainingData, testData
+
+def fullData():
+	trainingData =[]
+	testData =[] 
+	ids = reuters.fileids()
+	for i in ids:
+		if i.startswith('train'):
+			trainingData.append([_cleanData(reuters.raw(i)),reuters.categories(i)])
+		elif i.startswith('test'):
+			testData.append([_cleanData(reuters.raw(i)),reuters.categories(i)])
 	return trainingData, testData
 
 
@@ -33,4 +44,8 @@ categories = ['acq','earn','corn','crude']
 numberOfTraining = [114,152,38,76]
 numberOfTesting = [25,40,15,10]
 train, test = loadData(categories,numberOfTraining,numberOfTesting)
+print(len(train), len(test))
+
+print('Using fullData() takes a long time. We should maybe create a data dump for this?')
+train, test = fullData()
 print(len(train), len(test))
