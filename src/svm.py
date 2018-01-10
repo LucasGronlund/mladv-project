5c305d2c689c7379a757ed36803d5e51389adffa
Erik Rosvall <erik.rosvall@gmail.com>
2018-01-10 22:51:58 +0100

    försökt till v1.0 multiclass/multilabel svm genom sklearn, har fortfarande inte implementerat kernelmatris beräkningen

---
+++ src/svm.py
@@ -0,0 +1,35 @@
+import numpy as np
+from sklearn.svm import SVC
+from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.metrics import accuracy_score as a_s
+
+def _labelMaker(labels):
+	# Init labeling machine
+	mlb = MultiLabelBinarizer()
+	# Takes set of all possible labels, and creates a sparse matrix of 1/0 representations of class
+	return mlb.fit_transform(set(labels))
+
+def trainSVM(features, labels):
+	'''
+		This uses sklearn SVM kit using a one vs one approach.
+		One-vs-one is more computationally intensive than One-vs-all (n(n-1)/2 vs n)
+		but less sensitive to imbalanced data (which we do have, big time)
+	'''
+
+	Y = _labelMaker(labels)
+
+	#Compute Kernel matrix K, we need to precompute it for 
+	K = []
+
+	clf = SVC(kernel='precomputed')
+
+	# Compute the kernel matrix
+
+	return clf.fit(K,Y)
+
+def testSVM(features, classifier):
+	return classifier.predict(features)
+
+
+def accuracy(prediction,labels):
+	return a_s(_labelMaker(labels),prediction)
