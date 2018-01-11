544e221d89a27fe2bba7465029bec5ef15daf9c0
Viktor Karlsson <Viktor2k>
2018-01-11 21:27:15 +0100

    funktion för beräkning av alignmnent fixad

---
+++ src/alignment.py
@@ -0,0 +1,29 @@
+
+# coding: utf-8
+
+# In[21]:
+
+
+def alignment(A, B):
+    # A and B must have identical dimensions! dont be stupid trying anything else boi
+    if A.shape != B.shape:
+        print("Dimension error")
+        return 0
+    
+    AB = forbenius(A,B)
+    AA = forbenius(A,A)
+    BB = forbenius(B,B)
+    
+    return AB/np.sqrt(AA*BB)
+
+def forbenius(A, B):
+    if A.shape != B.shape:
+        print("Dimension error")
+        return 0
+    
+    res = 0
+    for i in range(A.shape[0]):
+        for j in range(A.shape[1]):
+            res += A[i,j]*B[i,j]            
+    return res
+
